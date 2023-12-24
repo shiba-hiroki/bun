a1873dd910639a07e5ad9f44b00ec81df00fb327
shibababa <h-shiba@digeon.co>
2023-12-24 10:24:08 +0900

    fix filename

--- pendulam/pendulam.py
+++ pendulam/pendulam.py
@@ -17,7 +17,7 @@ def derivs(t, state):
 t_span = [0, 20]
 dt = 0.05
 t = np.arange(t_span[0], t_span[1], dt)
-th1 = 30.0
+th1 = 180.0
 w1 = 0.0
 state = np.radians([th1, w1])
 
@@ -46,4 +46,4 @@ ax.set_xlim(-(L+1), L+1)
 ax.set_ylim(-(L+1), L+1)
 ax.set_aspect("equal")
 ax.grid()
-ani.save('pendulum.gif', writer='pillow', fps=15)
+ani.save('pendulum_180.gif', writer='pillow', fps=15)
